2100b56ab48a99a56305529ea8501cf1c3043358
Lamine005 <133173949+Lamine005@users.noreply.github.com>
2023-12-15 06:53:48 -0500

    modifier le moselaser method

--- index.js
+++ index.js
@@ -105,25 +105,29 @@ function shoot(e) {
   function moveLaser() {
     squares[currentLaserIndex].classList.remove('laser')
     currentLaserIndex -= width
-    squares[currentLaserIndex].classList.add('laser')
-
-    if (squares[currentLaserIndex].classList.contains('invader')) {
-      squares[currentLaserIndex].classList.remove('laser')
-      squares[currentLaserIndex].classList.remove('invader')
-      squares[currentLaserIndex].classList.add('boom')
-
-      setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+  
+    if (currentLaserIndex >= 0) {
+      squares[currentLaserIndex].classList.add('laser')
+  
+      if (squares[currentLaserIndex].classList.contains('invader')) {
+        squares[currentLaserIndex].classList.remove('laser')
+        squares[currentLaserIndex].classList.remove('invader')
+        squares[currentLaserIndex].classList.add('boom')
+  
+        setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 300)
+        clearInterval(laserId)
+  
+        const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+        aliensRemoved.push(alienRemoved)
+        results++
+        resultsDisplay.innerHTML = results
+        console.log(aliensRemoved)
+      }
+    } else {
       clearInterval(laserId)
-
-      const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
-      aliensRemoved.push(alienRemoved)
-      results++
-      resultsDisplay.innerHTML = results
-      console.log(aliensRemoved)
-
     }
-
   }
+  
   switch(e.key) {
     case 'ArrowUp':
       laserId = setInterval(moveLaser, 100)
